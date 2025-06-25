a013d55d776972412eadb1588c6223c906a5f3ce
Matt Redmond <10541289+redmond2742@users.noreply.github.com>
2025-06-24 18:43:28 -0700

    Update ProcessGPXMapper.vue

--- src/components/ProcessGPXMapper.vue
+++ src/components/ProcessGPXMapper.vue
@@ -166,7 +166,7 @@ export default {
         }
 
         const streetViewLink =
-          `https://www.google.com/maps/@${lat},${lon},3a,75y,${bearing || 0}h,90t/data=!3m7!1e1!3m5!1sjPxhPvbX83coQq-MEPy-ag!2e0!6shttps:%2F%2Fstreetviewpixels-pa.googleapis.com%2Fv1%2Fthumbnail%3Fcb_client%3Dmaps_sv.tactile%26w%3D900%26h%3D600%26pitch%3D0%26panoid%3DjPxhPvbX83coQq-MEPy-ag%26yaw%3D257.5721171177321!7i16384!8i8192?entry=ttu&g_ep=EgoyMDI1MDYxNy4wIKXMDSoASAFQAw%3D%3D`;
+          `https://www.google.com/maps/@?api=1&map_action=pano&viewpoint=lat,lng&heading=bearing&pitch=0&fov=80
 
         dataArray.push({
           Timestamp: time ? new Date(time).toLocaleString() : "",
